77077be90706a3911b6f7c82e7adbeeeaa8bfbb4
Tarasa24 <tarasa24@tarasa24.dev>
2022-09-08 23:06:07 +0200

    changing routing to all lower-case

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   get '/', to: 'index#get', as: 'index'
-  get 'AppDetail/:appID', to: 'app_detail#get', as: 'app_detail'
-  post 'Review/:appID', to: 'review#post', as: 'review'
-  get 'Reviews/:appID', to: 'review#get', as: 'reviews'
-  get 'Search', to: 'search#get', as: 'search'
-  get 'Author/:authorID', to: 'author#get', as: 'author'
+  get 'appdetail/:appID', to: 'app_detail#get', as: 'app_detail'
+  post 'review/:appID', to: 'review#post', as: 'review'
+  get 'reviews/:appID', to: 'review#get', as: 'reviews'
+  get 'search', to: 'search#get', as: 'search'
+  get 'author/:authorID', to: 'author#get', as: 'author'
 end
